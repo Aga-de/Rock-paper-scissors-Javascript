02ea7c7359517c2717a16244c176eba9ef021d64
Aga Daniuk <agdani@wp.pl>
2020-11-07 12:10:37 +0100

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+const playGame = function(playerInput) {
 
-  
+  clearMessages();
+
   let randomNumber = Math.floor(Math.random() * 3 + 1);
 
   const getMoveName = function(randomNumber) {
@@ -25,7 +27,7 @@
   printMessage('Mój ruch to: ' + computerMove);
 
 
-  let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+  //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
   console.log('Gracz wpisał: ' + playerInput);
 
@@ -89,5 +91,15 @@
       return 'Błędnie wpisany ruch!';
     }
   }
-
   displayResult(argComputerMove,argPlayerMove);
+} 
+
+document.getElementById('play-rock').addEventListener('click', function(){
+  playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function(){
+  playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
+  playGame(3);
+});
